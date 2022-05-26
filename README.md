c982adfc43a304c768f8b50653f77a982addaba3
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-05-26 20:33:11 +0200

    feat: angular app init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# dislinkt-frontend
+# DISLINKT
+
+### Where start your Professional Experience.
+
+<br>
+
+![](Dislinkt_logo.png)
+
+<br>
+
+This project has been done by:
+
+    1. Aleksander.
+    2. Darko.
+    3. Alejandro Fernández Pérez.
+    4. Leki.
+
+<br>
+
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Home{
+  title:string;
+  subtitle:string;
+  image:string;
+}
+
+@Component  ({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent implements OnInit {
+  title = 'Last Notices';
+  public Notices: Home[]=[];
+  
+  ngOnInit(): void {
+
+    this.Notices = [
+     {title:'Engineer Job', subtitle: 'Do you like the Networks & Electronic? This is your Job', image:"https://i.ytimg.com/vi/PSD5pFi6bx4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBRXrJxTLALTsBas1djapMjtW0lQg"},
+     {title:'Web Developer Job', subtitle: 'Creativity, ingenuity, to resolve problems.... Are you ready?', image:"https://i.ytimg.com/vi/rOsd3OI9dOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdMDLcELCLzlSzVoELr-QZ41mL8g"},
+     {title:'Lifeguard Job for Summer Work & Travel', subtitle: 'Work with as and have a great Summer in USA', image:"https://i.ytimg.com/vi/lasGrvRYBWg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDgh8fipV-k2f4xjEEPpvvtiWHwjA"},
+    ]
+ }
+}

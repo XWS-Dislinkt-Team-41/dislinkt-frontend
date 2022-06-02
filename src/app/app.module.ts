8f9a9ef18e5e7fc2817df787fc9e1117521baff9
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-06-02 11:45:28 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-
-export function createTranslateLoader(http: HttpClient){
-  return new TranslateHttpLoader(http, './assets/', '.json')
-}
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    TranslateModule.forRoot(
-      loader:{
-        provide: TranslateLoader,
-        useFactory: (createTranslateLoader),
-        deps: [HttpClient]
-      }
-    ) 
-  ],
-  bootstrap: [AppComponent],
 })
 
 export class AppModule { }

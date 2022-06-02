9d5247248c187cac193e170e660b7a3ca1f59e7a
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-06-03 00:01:49 +0200

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 interface Home{
   title:string;
@@ -23,6 +24,10 @@ export class AppComponent implements OnInit {
      {title:'Web Developer Job', subtitle: 'Creativity, ingenuity, to resolve problems.... Are you ready?', image:"https://i.ytimg.com/vi/rOsd3OI9dOA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBdMDLcELCLzlSzVoELr-QZ41mL8g"},
      {title:'Lifeguard Job for Summer Work & Travel', subtitle: 'Work with as and have a great Summer in USA', image:"https://i.ytimg.com/vi/lasGrvRYBWg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDgh8fipV-k2f4xjEEPpvvtiWHwjA"},
     ]
-
  }
+
+  constructor(private translate: TranslateService) {    
+      translate.setDefaultLang('en');    
+      translate.use('en');  
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+// import ngx-translate and the http loader.
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
-  ],
+    HomeComponent,
+    ProfileComponent,
+    SectionComponent ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    // ngx-translate and the loader module.
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }        
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// required for AOT compilation.
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {    
+    return new TranslateHttpLoader(http);
+}

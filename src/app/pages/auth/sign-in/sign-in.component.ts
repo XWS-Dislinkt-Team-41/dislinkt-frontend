422931ee3107dafe12a6e0f6e6f39b40db07bb1e
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2022-06-05 20:14:41 +0200

    fix Login Dislinkt

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OptionsForm } from '@auth/form/form.component';
-import { ACTIONS } from '@shared/navbar/Consts/Consts';
+import { ACTIONS } from '@shared/constants/constant';
 
 @Component({
   selector: 'app-sign-in',
